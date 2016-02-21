0786147f8f88cb0b7b2be40ccc53f1ec978462ea
olipeturolsen98 <oliolsen@outlook.com>
2016-02-21 21:04:50 +0000

    sida3

---
+++ vefsida/include/menu.php
@@ -0,0 +1,15 @@
+<?php $currentPage = basename($_SERVER['SCRIPT_FILENAME']); ?>
+<nav>
+	<h3>Sites:</h3>
+	<ul>
+		<li>
+			<a href="index.php" <?php if ($currentPage == "index.php") { echo "class=\"current\""; } ?>>Forsíða</a>
+		</li>
+		<li>
+			<a href="ummig.php" <?php if ($currentPage == "ummig.php") { echo "class=\"current\""; } ?>>Um mig</a>
+		</li>
+		<li>
+			<a href="login.php" <?php if ($currentPage == "login.php") { echo "class=\"current\""; } ?>>Login/Register</a>
+		</li>
+	</ul>
+</nav>
