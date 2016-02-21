6f1a458ac7773261f032d50cd0fcfe2fab5fc117
olipeturolsen98 <oliolsen@outlook.com>
2016-02-21 20:33:42 +0000

    sida2

--- vefsida/index.php
+++ vefsida/index.php
@@ -1,4 +1,4 @@
-
+<!--
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
@@ -139,5 +139,24 @@
                 <script>
                     $(document).foundation();
                 </script>
+</body>
+</html>
+-->
+<?php
+    session_start(); 
+ ?>
+<?php include'./includes/title.php';?>
+<!DOCTYPE html>
+<?php  include("./includes/head.php");?>
+<body>
+    <?php include("./includes/header.php") ?>
+<div class="containall">
+    <?php include("./includes/menu.php") ?>
+    <main>
+        <?php include("./includes/images.php") ?>
+    </main>
+</div>
+<?php include("./includes/footer.php") ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </body>
 </html>
