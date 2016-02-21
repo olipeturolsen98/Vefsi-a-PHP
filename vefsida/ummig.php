a5ce2107deca17e3e5dbfaad5b9d4366eb866411
olipeturolsen98 <oliolsen@outlook.com>
2016-02-21 20:22:58 +0000

    sidur

---
+++ vefsida/ummig.php
@@ -0,0 +1,18 @@
+<?php
+    session_start(); 
+ ?>
+<?php include'./includes/title.php';?>
+<!DOCTYPE html>
+<?php  include("./includes/head.php");?>
+<body>
+    <?php include("./includes/header.php") ?>
+<div class="containall">
+    <?php include("./includes/menu.php") ?>
+    <main>
+        <?php include("./includes/images.php") ?>
+    </main>
+</div>
+<?php include("./includes/footer.php") ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</body>
+</html>
