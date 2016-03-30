d740a00bfdb6ea25d927efe10777d1d494a4f921
olipeturolsen98 <oliolsen@outlook.com>
2016-03-30 16:44:36 +0000

    hehe

--- vefsida/ummig.php
+++ vefsida/ummig.php
@@ -1,26 +1,18 @@
 <?php
     session_start(); 
- ?>
-<?php include'./include/title.php';?>
+?>
+<?php include'./includes/title.php';?>
 <!DOCTYPE html>
-<?php  include("./include/head.php");?>
+<?php  include("./includes/head.php");?>
 <body>
-    <?php include("./include/header.php") ?>
-    <div class="containall">
-    <?php include("./include/menu.php") ?>
+    <?php include("./includes/header.php") ?>
+<div class="containall">
+    <?php include("./includes/menu.php") ?>
     <main>
-         <div class="row">
-                <div class="medium-6 columns">
-                    <h3>Hérna eru upplýsingar um mig og leiki</h3>
-                    <img src="img\oli.jpg" class="thumbnail">
-                    <h5>Óli Olsen</h5>
-                </div>
+        <p>Texti hér</p>
     </main>
-<?php include("./include/footer.php") ?>
+</div>
+<?php include("./includes/footer.php") ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-                <script src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
-                <script>
-                    $(document).foundation();
-                </script>
 </body>
 </html>
