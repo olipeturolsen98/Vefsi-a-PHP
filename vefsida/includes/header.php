4b33755339fb4ea3b105a03edf4bca3397af3d36
olipeturolsen98 <blida12345>
2016-09-14 14:42:27 +0000

    dsf

--- vefsida/includes/header.php
+++ vefsida/includes/header.php
@@ -32,7 +32,7 @@
     $qoutes = array("We need to turn the power on! It's like deja vu all over again." , "Whoo! More shit to kill with!", "NO! NO VODKA FOR YOU!" , "Out of my way, suka!" , "I will show them the path!" , "You kill me? No! I kill you." , "Do you feel it? Oh I hope they feel it..." , "I'm going to kill SOOOOO many now." );
     shuffle($qoutes);
   ?>
-<header class="custom-wrapper pure-g" id="menu" style="background-image: url('<?php echo $imgpath; ?>')">
+<header class="custom-wrapper pure-g" id="menu" style="background-image: url('<?php echo $images; ?>')">
 	<div>
 		<a href="index.php" class="headerlink"><h1 class="_i_myndun">Call of Duty Zombies</i></h1></a><br>
 	</div>
