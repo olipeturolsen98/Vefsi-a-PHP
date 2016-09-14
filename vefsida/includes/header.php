c5a1ed5a45d3c4bdcf47a2772fd7087f1cca207e
olipeturolsen98 <blida12345>
2016-09-14 14:39:15 +0000

    BACKround img

--- vefsida/includes/header.php
+++ vefsida/includes/header.php
@@ -1,12 +1,34 @@
 <?php
 //Velur random mynd í header
-    $dir = 'img/header/';
+   /* $dir = 'img/header/';
     if (isset($_SESSION["files"]) == false || count($_SESSION["files"]) == 0) {
     	$_SESSION["files"] = glob("{$dir}*.{jpg,png,gif,jpeg}", GLOB_BRACE);
     }
     shuffle($_SESSION["files"]);
     $imgpath = $_SESSION["files"][0];
-    unset($_SESSION["files"][0]);
+    unset($_SESSION["files"][0]);*/
+
+      $images = [
+    ['file'    => 'indexZ/Call-Of-Duty-Zombies-Wallpaper-02',
+     'caption' => 'Mob of the dead'],
+    ['file'    => 'indexZ/Call-Of-Duty-Zombies-Wallpaper-05',
+     'caption' => 'Classic zombies'],
+    ['file'    => 'indexZ/Call-Of-Duty-Zombies-Wallpaper-07',
+     'caption' => 'Moon'],
+    ['file'    => 'indexZ/Call-Of-Duty-Zombies-Wallpaper-10',
+     'caption' => 'Perkaholic'],
+    ['file'    => 'indexZ/Call-Of-Duty-Zombies-Wallpaper-11',
+     'caption' => 'Buried'],
+    ['file'    => 'indexZ/Call-Of-Duty-Zombies-Wallpaper-12',
+     'caption' => 'die rise'],
+    ];
+    $i = rand(0, count($images)-1);
+    $selectedImage = "images/{$images[$i]['file']}.jpg";
+    $caption = $images[$i]['caption'];
+    if (file_exists($selectedImage) && is_readable($selectedImage)) {
+    $imageSize = getimagesize($selectedImage);
+    }
+
     $qoutes = array("We need to turn the power on! It's like deja vu all over again." , "Whoo! More shit to kill with!", "NO! NO VODKA FOR YOU!" , "Out of my way, suka!" , "I will show them the path!" , "You kill me? No! I kill you." , "Do you feel it? Oh I hope they feel it..." , "I'm going to kill SOOOOO many now." );
     shuffle($qoutes);
   ?>
