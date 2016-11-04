38cdea5f90f0613661f6d888a8d717f8ebb3a47e
olipeturolsen98 <blida12345>
2016-11-04 09:43:19 +0000

    s

--- vefsida/ummig.php
+++ vefsida/ummig.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['username'])){
     echo "you logged in as </br>", $_SESSION['username'];
-    echo "<br/><a href='logout.php'>logout</a>";
+    
 }else{
 //your page stuff
 } 
